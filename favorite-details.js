ae19edf9cedba3e9a46fd4f5e3bcd7ccd506a0d2
ben-trimark <bray@trimarkusa.com>
2024-07-24 10:13:36 -0400

    new file favorite-details

---
+++ favorite-details.js
@@ -0,0 +1,25 @@
+/* create Mutation Observer */
+const config = { childList: true, characterData: false, subtree: true, attributes: true };
+
+const callback = function (mutationsList, observer) {
+  updateWarningText();
+};
+
+const observer = new MutationObserver(callback);
+var targetNode = document.body;
+if (targetNode) {
+  observer.observe(targetNode, config);
+}
+/* end Mutation Observer */
+
+updateWarningText = () => {
+  /* replace "currently not in stock" with "low stock" */
+  const warningMessage = $('.message.warning');
+  warningMessage.map( (warningDiv, i) => {
+     const lowStockDiv = $('.message.warning')[warningDiv];
+     if ( $(lowStockDiv).html() == "Currently not in stock" ) {
+       $(lowStockDiv).html("Low")
+     }
+  });
+
+}
